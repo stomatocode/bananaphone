810c57a1c705895a3af12268d0b1d1a8fc573b63
stomatocode <dewrgleision@gmail.com>
2014-11-06 08:12:22 +0000

    add gpio library; require gpio in client script; generate package.json for cli utility

--- client.js
+++ client.js
@@ -8,10 +8,10 @@ var accountSid = 'AC94c28a23360c8d180b576c01539028be';
 var authToken = "89f3c0bce24cf40e9e37473cbc097b6a";
 var client = twilio(accountSid, authToken);
 
-// just makes a call to my phone right now
+// just makes a call to a phone right now
 
 	client.makeCall({
-	  to: '+17072257359',
+	  to: '+13039182097',
 	  from: '+17076746183',
 	  url: 'http://twimlets.com/message?Message%5B0%5D=http%3A%2F%2Fwww.directlinkupload.com%2Fuploads%2F50.161.13.11%2FbananaPhone.mp3&'
 	}, function(err, responseData) { 
