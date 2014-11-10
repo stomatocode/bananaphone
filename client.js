bdc118d279675ca2106bc53091dfc6bd7e3f144e
stomatocode <dewrgleision@gmail.com>
2014-11-10 15:00:25 -0800

    add untracked files to git ignore; add some test code from restify to test my local connection

---
+++ client.js
@@ -0,0 +1,26 @@
+// Made using Restify.js: http://mcavage.me/node-restify/#server-api
+
+// test server code
+
+// var restify = require('restify');
+//
+// function respond(req, res, next) {
+//   res.send('hello ' + req.params.name);
+//   next();
+// }
+//
+// var server = restify.createServer();
+// server.get('/hello/:name', respond);
+// server.head('/hello/:name', respond);
+//
+// server.listen(8080, function() {
+//   console.log('%s listening at %s', server.name, server.url);
+// });
+
+
+// pre function: executes before routing occurs
+
+// server.pre(function(req, res, next) {
+//   req.headers.accept = 'application/json';  // screw you client!
+//   return next();
+// });
